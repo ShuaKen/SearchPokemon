5272030031b7b1245b7188fe1fdba0007a78c741
ShuaKen <joshu.887@gmail.com>
2024-01-05 16:26:58 -0300

    finished

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/service'),
+      dir: require('path').join(__dirname, './coverage/c10'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>Encontre o Pokemon</h1>
+  <app-card></app-card>
+  <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'service';
+  title = 'C10';
 }
